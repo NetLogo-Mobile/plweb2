21ebd1cba401b7d31980adf823e011321fd913aa
wsxiaolin <wsxiaolin@users.noreply.github.com>
2026-04-26 06:56:52 +0000

    deploy: 1fd60a8b2b0389283829ea156a355bb04adebd71

--- assets/Navigation-Return-Bc2jLT3X.js
+++
@@ -1,1 +0,0 @@
-const s="/plweb2/assets/library/Navigation-Return.png";export{s as _};
--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/plweb2/sw.js', { scope: '/plweb2/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
