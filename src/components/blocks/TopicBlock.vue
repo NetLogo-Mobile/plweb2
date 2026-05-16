b8c75fe8683d9190058e36bc1ef2e34193c60726
gushishang <117088703+gushishang@users.noreply.github.com>
2026-05-16 08:49:15 +0800

    FIx： unencoded target link in  topic block

--- src/components/blocks/TopicBlock.vue
+++ src/components/blocks/TopicBlock.vue
@@ -34,7 +34,7 @@
 import { useRouter } from 'vue-router'
 import type { TopicBlock as TopicBlockType } from '@services/../pl-serve-type-main/type/main'
 import Works from '../projects/brief.vue'
-import { getCoverUrl, getPath } from '@services/utils.ts'
+import { getCoverUrl, getPath ,EncodeAPITargetLink} from '@services/utils.ts'
 
 const router = useRouter()
 
@@ -48,7 +48,7 @@ const props = withDefaults(defineProps<Props>(), {
 })
 
 const handleContainerClick = () => {
-  router.push(`/l/${props.block.TargetLink}`)
+  router.push((`/l/${EncodeAPITargetLink(props.block.TargetLink)}`))
 }
 </script>
 
